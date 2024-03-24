adeb84fd1ff5c054d085c6c0bc6ddee9bba04cbd
Serene Mathew <46328829+hatt3r@users.noreply.github.com>
2024-03-24 22:49:24 +0530

    final with comments

--- index.js
+++ index.js
@@ -7,8 +7,11 @@ const {MongoClient} = require('mongodb')
 const dns = require('dns');
 // Basic Configuration
 const port = process.env.PORT || 3000;
+//connecting to db using MongoClient with URI as argument
 const cdb = new MongoClient(process.env.MONGO_URI);
+//a way to use the instance of the database that is connected
 const db = cdb.db("url_service");
+//a variable is created to show the collection of urls collected in the database
 const storeUrls = db.collection('urls');
 app.use(cors());
 
@@ -25,17 +28,25 @@ app.get('/', function(req, res) {
 // Your first API endpoint
 app.post('/api/shorturl', function(req, res){
   const urlString = req.body.url;
+  //using urlparser to parse the string in the url and return URL object
   const lookDns = dns.lookup(urlparser.parse(urlString).hostname , async(req , validAddress) => {
+  //if valid Adress does not exist then response json as error:"Invalid URL"
    if(!validAddress){
     res.json({error: "Invalid URL"})
-   } else {
+   }
+   //if valid address exist
+    else {
+    //count the number of urls in the database collection of urls called storeUrls
     const countUrls = await storeUrls.countDocuments({});
+    //new variable urlstore containing urlstring and short_url that is number of urls countUrls
     const urlStore = {
       urlString,
       short_url: countUrls
     }
-   const respond = await storeUrls.insertOne(urlStore);
+    //the main array storeUrls will be inserted with url being looked at the moment
+    const respond = await storeUrls.insertOne(urlStore);
     console.log(respond);
+    //final response json will be original_url :with urlString and short url with number of urls
     res.json({
       original_url: urlString,
       short_url: countUrls
@@ -43,9 +54,14 @@ app.post('/api/shorturl', function(req, res){
    }
   })
  });
+
+ 
 app.get("/api/shorturl/:short_url" , async (req ,res) => {
+  //if shorturl exists then new variable shortUrl is created from req.params.short_url
   const shorturl = req.params.short_url;
+  //if multiple number then finding and increasing the shortUrl value and putting in the url Store
   const urlStore = await storeUrls.findOne({ short_url: +shorturl })
+  //redirecting to urlstring in the urlStore
   res.redirect(urlStore.urlString); 
 })
 
