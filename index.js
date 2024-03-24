9bfb13f7309b7e150dbc26c28d68a11b3fc5c3aa
Serene Mathew <46328829+hatt3r@users.noreply.github.com>
2024-03-24 20:04:49 +0530

    update

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use('/public', express.static(`${process.cwd()}/public`));
 app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
-
+//new
 // Your first API endpoint
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
